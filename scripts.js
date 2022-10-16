b231088f5a5eb534b1e4306e56eab08f825b6d13
Bruno Stefano de Souza <40743118+vonkoln@users.noreply.github.com>
2022-10-16 01:22:30 -0300

    UploadDeArquivos

---
+++ scripts.js
@@ -0,0 +1,24 @@
+const handleSubmit = (event) => {
+    event.preventDefaut();
+
+    const firstname = document.querySelector('input[name=firstname]').value;
+    const lastname = document.querySelector('input[name=lastname]').value;
+    const email = document.querySelector('input[name=email]').value;
+    const number = document.querySelector('input[name=number]').value;
+
+
+    fetch('https://api.sheetmonkey.io/form/acuQdn5GVSTyVCM8iVpQEB', {
+
+    method: 'post',
+    headers: {
+        'Accept': 'application/jason',
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ firstname, lastname, email, number }),
+   });
+}
+
+document.querySelector('form').addEventListener('submit', handleSubmit);
+
+
+
