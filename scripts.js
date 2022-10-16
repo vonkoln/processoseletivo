07bf3b05361a94b8c7b73cdf0a13f871426e28dc
Bruno Stefano de Souza <40743118+vonkoln@users.noreply.github.com>
2022-10-16 19:34:33 -0300

    nascimento

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ const handleSubmit = (event) => {
     const lastname = document.querySelector('input[name=lastname]').value;
     const email = document.querySelector('input[name=email]').value;
     const number = document.querySelector('input[name=number]').value;
-
+    const nascimento = document.querySelector('input[name=nascimento]').value;
 
     fetch('https://api.sheetmonkey.io/form/acuQdn5GVSTyVCM8iVpQEB', {
 
@@ -14,7 +14,7 @@ const handleSubmit = (event) => {
         'Accept': 'application/jason',
         'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ firstname, lastname, email, number }),
+    body: JSON.stringify({ firstname, lastname, email, number, nascimento }),
    });
 }
 
